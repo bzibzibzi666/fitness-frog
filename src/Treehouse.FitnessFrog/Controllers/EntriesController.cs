a8a62a44bff0706ab43348084482ebdfacdc2fd2
bzibzibzi666 <bzibzibzi666@wp.pl>
2018-03-05 21:37:58 +0100

    adding validation rules - data annotation

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -52,6 +52,11 @@ namespace Treehouse.FitnessFrog.Controllers
         [HttpPost]
         public ActionResult Add(Entry entry)
         {
+            if(ModelState.IsValidField("Duration") && entry.Duration <= 0)
+            {
+                ModelState.AddModelError("Duration", "Duration field must be > 0 ");
+            }
+
             if(ModelState.IsValid)
             {
                 _entriesRepository.AddEntry(entry);
